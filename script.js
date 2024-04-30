33172d25966dd0cb8d41ecb59982fe8583cbae37
eyahakimi223 <eyahakimi223@gmail.com>
2024-04-30 22:01:46 +0200

    fixing

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
     const thumbnails = document.querySelectorAll('.thumbnail');
     const slideshowImages = document.querySelectorAll('#slideshow img');
     const prevButton = document.getElementById('previous-page');
     const playButton = document.getElementById('play');
-    const prev = document.getElementById('prev'); 
+    const prev = document.getElementById('prev');
     const next = document.getElementById('next');
     const randomButton = document.getElementById('random');
     const nextButton = document.getElementById('next-page');
     const toggleToolbarButton = document.getElementById('toggle-toolbar');
-    const toolbar = document.getElementById('toolbar'); 
+    const toolbar = document.getElementById('toolbar');
     const thumbnailsPerPage = 5;
     let currentPage = 0;
     let intervalId = null;
@@ -18,26 +17,20 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Showing slide with index:', slideIndex);
         slideshowImages.forEach(image => image.classList.remove('active'));
         thumbnails.forEach(thumbnail => thumbnail.classList.remove('active'));
-    
         slideshowImages[slideIndex].classList.add('active');
         thumbnails[slideIndex].classList.add('active');
-    
-        // Hide content of all slides
         const contents = document.querySelectorAll('#slideshow .content');
         contents.forEach(content => {
             content.style.display = 'none';
         });
-    
-        // Show content of current slide
         const currentSlide = slideshowImages[slideIndex];
         const currentContent = currentSlide.nextElementSibling;
         currentContent.style.display = 'block';
     }
-    
     function showThumbnails(pageIndex) {
         const startIndex = pageIndex * thumbnailsPerPage;
         const endIndex = Math.min(startIndex + thumbnailsPerPage, slideshowImages.length);
-    
+
         for (let i = 0; i < slideshowImages.length; i++) {
             if (i >= startIndex && i < endIndex) {
                 thumbnails[i].style.display = 'block';
@@ -46,7 +39,6 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     }
-    
     function toggleToolbar() {
         toolbar.style.display = (toolbar.style.display === 'none' || toolbar.style.display === '') ? 'block' : 'none';
         prevButton.style.display = toolbar.style.display === 'none' ? 'block' : 'none';
@@ -55,29 +47,16 @@ document.addEventListener('DOMContentLoaded', function () {
             thumbnails.forEach((thumbnail, index) => {
                 thumbnail.style.display = 'none';
             });
-            // Ajoutez la classe pour modifier l'icône vers le bas
             toggleToolbarButton.classList.add('down-arrow');
-            // Supprimez la classe pour réinitialiser l'icône vers la droite
             toggleToolbarButton.classList.remove('right-arrow');
         } else {
             thumbnails.forEach((thumbnail, index) => {
                 thumbnail.style.display = index < 5 ? 'block' : 'none';
             });
-            // Supprimez la classe pour réinitialiser l'icône vers le bas
             toggleToolbarButton.classList.remove('down-arrow');
-            // Ajoutez la classe pour modifier l'icône vers la droite
             toggleToolbarButton.classList.add('right-arrow');
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-
     function nextSlide() {
         currentSlide++;
         if (currentSlide >= slideshowImages.length) {
@@ -85,7 +64,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         showSlide(currentSlide);
     }
-
     function prevSlide() {
         currentSlide--;
         if (currentSlide < 0) {
@@ -93,7 +71,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         showSlide(currentSlide);
     }
-
     thumbnails.forEach((thumbnail, index) => {
         thumbnail.addEventListener('click', function () {
             currentSlide = index;
@@ -105,12 +82,11 @@ document.addEventListener('DOMContentLoaded', function () {
             intervalId = setInterval(nextSlide, 3000); // Change 3000 to desired interval in milliseconds
         }
     }
-
     function stopPlay() {
         clearInterval(intervalId);
         intervalId = null;
     }
-    document.addEventListener('keydown', function(event) {
+    document.addEventListener('keydown', function (event) {
         if (event.code === 'Space') {
             if (intervalId) {
                 stopPlay();
@@ -130,7 +106,6 @@ document.addEventListener('DOMContentLoaded', function () {
         do {
             randomIndex = Math.floor(Math.random() * slideshowImages.length);
         } while (randomIndex === currentSlide);
-    
         currentSlide = randomIndex;
         showSlide(currentSlide);
     }
@@ -142,14 +117,13 @@ document.addEventListener('DOMContentLoaded', function () {
             showThumbnails(currentPage);
         }
     });
-
     prevButton.addEventListener('click', function () {
         if (currentPage > 0) {
             currentPage--;
             showThumbnails(currentPage);
         }
     });
-    playButton.addEventListener('click', function() {
+    playButton.addEventListener('click', function () {
         if (intervalId) {
             stopPlay();
             playButton.innerHTML = '<i class="fas fa-play"></i>';
@@ -158,15 +132,8 @@ document.addEventListener('DOMContentLoaded', function () {
             playButton.innerHTML = '<i class="fas fa-pause"></i>';
         }
     });
-    randomButton.addEventListener('click', randomSlide); // Attach event listener to random button
-
-    prev.addEventListener('click', prevSlide); // Attach event listener to prev button
-
-    // Initial display of thumbnails
+    randomButton.addEventListener('click', randomSlide); 
+    prev.addEventListener('click', prevSlide); 
     showThumbnails(currentPage);
-
-    // Initial display of the first slide
     showSlide(currentSlide);
-
     next.addEventListener('click', nextSlide);
-});
